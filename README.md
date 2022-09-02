aedf1b9fc8e8b426b976fd15d6669adc97a29faf
Luca <lucaandrey5@hotmail.com>
2022-09-02 13:34:56 -0300

    Corrigindo bugs iniciais do projeto

--- README.md
+++
@@ -1,2 +0,0 @@
-# Ecommerce-SolarEnergy
-Ecommerce da energia solar
---
+++ solarenergy/src/main/java/com/generation/solarenergy/models/CategoriaModels.java
@@ -0,0 +1,51 @@
+package com.generation.solarenergy.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_categorias")
+public class CategoriaModels {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "O atributo eh obrigatorio")
+    @Size(min = 5, max = 255)
+    private String tipoUsuario;
+    
+    @NotBlank(message = "O atributo eh obrigatorio")
+    @Size(min = 5, max = 255)
+    private String tipoProduto;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTipoUsuario() {
+        return tipoUsuario;
+    }
+
+    public void setTipoUsuario(String tipoUsuario) {
+        this.tipoUsuario = tipoUsuario;
+    }
+
+    public String getTipoProduto() {
+        return tipoProduto;
+    }
+
+    public void setTipoProduto(String tipoProduto) {
+        this.tipoProduto = tipoProduto;
+    }
+
+}
