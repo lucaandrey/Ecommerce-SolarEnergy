4a6123226fa2e3e39de711c43bfa4486f52320c1
Luca <lucaandrey5@hotmail.com>
2022-09-02 12:06:57 -0300

    Criando projeto e implementando model de categoria

---
+++ src/main/java/com/generation/solarenergy/SolarenergyApplication.java
@@ -0,0 +1,13 @@
+package com.generation.solarenergy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolarenergyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolarenergyApplication.class, args);
+	}
+
+}
